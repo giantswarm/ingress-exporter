9df41b5bea811ad84026644dd29db8868e6a8265
Vaclav Rozsypalek <vaclav@giantswarm.io>
2019-05-10 14:14:27 +0200

    inititalise g8s config properly

--- flag/service/service.go
+++ flag/service/service.go
@@ -1,9 +1,12 @@
 package service
 
 import (
+	"github.com/giantswarm/operatorkit/flag/service/kubernetes"
+
 	"github.com/giantswarm/ingress-exporter/flag/service/ingress"
 )
 
 type Service struct {
 	Ingress ingress.Ingress
+	Kubernetes     kubernetes.Kubernetes
 }
--- service/service.go
+++ service/service.go
@@ -71,10 +71,17 @@ func New(config Config) (*Service, error) {
 		var restConfig *rest.Config
 		{
 			c := k8srestconfig.Config{
-				Logger:    config.Logger,
-				InCluster: true,
-			}
+				Logger: config.Logger,
 
+				Address:    config.Viper.GetString(config.Flag.Service.Kubernetes.Address),
+				InCluster:  config.Viper.GetBool(config.Flag.Service.Kubernetes.InCluster),
+				KubeConfig: config.Viper.GetString(config.Flag.Service.Kubernetes.KubeConfig),
+				TLS: k8srestconfig.ConfigTLS{
+					CAFile:  config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CAFile),
+					CrtFile: config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.CrtFile),
+					KeyFile: config.Viper.GetString(config.Flag.Service.Kubernetes.TLS.KeyFile),
+				},
+			}
 			restConfig, err = k8srestconfig.New(c)
 			if err != nil {
 				return nil, microerror.Mask(err)
