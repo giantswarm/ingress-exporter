f1caba05b256431e6a713c7ce632eb6e611d66cb
Jakub Soboń <wtty.fool@gmail.com>
2020-05-13 18:19:07 +0200

    bump version to 0.1.1-dev (#13)

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "ingress-exporter"
 	source      = "https://github.com/giantswarm/ingress-exporter"
-	version     = "0.1.0"
+	version     = "0.1.1-dev"
 )
 
 func Description() string {
