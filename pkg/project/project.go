e2dd90cb798eecaac91454139299988b1604ca35
GitHub Action <action@github.com>
2021-08-13 14:34:46 +0000

    Bump version to 1.1.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "ingress-exporter"
 	source      = "https://github.com/giantswarm/ingress-exporter"
-	version     = "1.1.0"
+	version     = "1.1.1-dev"
 )
 
 func Description() string {
