2a7ab28502262b8051877c3b95221a12ffc1d680
taylorbot <dev@giantswarm.io>
2023-05-31 14:49:09 +0000

    Bump version to 1.1.13-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "ingress-exporter"
 	source      = "https://github.com/giantswarm/ingress-exporter"
-	version     = "1.1.12"
+	version     = "1.1.13-dev"
 )
 
 func Description() string {
