bbf3442276ff3ef6935faa7b5c3096e2441b9cc8
taylorbot <dev@giantswarm.io>
2023-10-17 08:10:14 +0000

    Release v1.2.0

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -5,7 +5,7 @@ var (
 	gitSHA      = "n/a"
 	name        = "ingress-exporter"
 	source      = "https://github.com/giantswarm/ingress-exporter"
-	version     = "1.1.15-dev"
+	version     = "1.2.0"
 )
 
 func Description() string {
